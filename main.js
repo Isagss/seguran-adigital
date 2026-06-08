fd9016c52f1d6e42da4c86062d65c65856d4acb7
Isagss <s.isabela25@escola.pr.gov.br>
2026-06-08 10:50:07 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+const numeroSenha= document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+
+cont botoes = document.querySelectorAll('.parametro-senha__botao');
+
+botoes[0].onclik = diminuiTamanho;
+
+function diminuiTamanho(){
+tamanhoSenha = tamanhoSenha-1;
+numeroSenha.textContent = tamanhoSenha
+}
+console.log(botoes);
