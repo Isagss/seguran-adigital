df70b2e357908cac9579ea7bb0b25db9dc26df46
Isagss <s.isabela25@escola.pr.gov.br>
2026-06-22 10:39:56 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -5,9 +5,38 @@ numeroSenha.textContent = tamanhoSenha;
 cont botoes = document.querySelectorAll('.parametro-senha__botao');
 
 botoes[0].onclik = diminuiTamanho;
+botoes[1].onclik = aumentoTamanho;
 
 function diminuiTamanho(){
-tamanhoSenha = tamanhoSenha-1;
+    if(tamanhoSenha > 1){
+       // tamanhoSenha = tamanhoSenha-1;
+        tamanhoSenha--;
+    }
 numeroSenha.textContent = tamanhoSenha
+geraSenha();
 }
-console.log(botoes);
+fuction aumentaTamanho(){
+    if(tamanhoSenha < 20){
+       // tamanhoSenha = tamanhoSenha+1;
+        tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamnhoSenha;
+    geraSenha();
+}
+
+const campoSenha = document.querySelector('#campo-senha);
+
+const letrasMaiuscula = 'ABCDFGHIJKLMNOPQRSTUVWXYZ';
+geraSenha();
+
+function geraSenha(){
+let senha = '';
+    for (let i = 0; i < tamnhoSenha;++){
+    let numeroAleatorio = Math.random()*letrasMaiuscula.length;
+    numeroAletorio = Math.floor(numeroAleatorio);
+    senha = senha + letrasMaiusculas[numeroAleatorio]/
+   }
+campoSenha.value = senha;
+}
+
+
